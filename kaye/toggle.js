ffe5f317ebab2e6c0aef9b8ff87b2a803fe78e9c
Phoebe-23 <pangankaye55@gmail.com>
2025-02-07 14:52:28 +0800

    Add files via upload
    updated toggle

--- kaye/toggle.js
+++ kaye/toggle.js
@@ -1,16 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
+    const toggleWrapper = document.getElementById("toggle-wrapper");
     const toggleOn = document.getElementById("toggle-on");
     const toggleOff = document.getElementById("toggle-off");
 
-    toggleOn.addEventListener("change", function () {
+    toggleWrapper.addEventListener("click", function () {
         if (toggleOn.checked) {
-            console.log("Switched to On");
-        }
-    });
-
-    toggleOff.addEventListener("change", function () {
-        if (toggleOff.checked) {
+            toggleOff.checked = true;
             console.log("Switched to Off");
+        } else {
+            toggleOn.checked = true;
+            console.log("Switched to On");
         }
     });
 });

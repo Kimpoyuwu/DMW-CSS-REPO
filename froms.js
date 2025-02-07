7e9823ff6ceaea36bb0f7625819d833e5068d9ec
Phoebe-23 <pangankaye55@gmail.com>
2025-02-07 14:42:41 +0800

    Add files via upload
    added form html,css

---
+++ froms.js
@@ -0,0 +1,17 @@
+const modal = document.getElementById("formModal");
+const openModalBtn = document.getElementById("openModal");
+const closeModalBtn = document.querySelector(".close");
+
+openModalBtn.addEventListener("click", () => {
+  modal.style.display = "flex";
+});
+
+closeModalBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+window.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+});
